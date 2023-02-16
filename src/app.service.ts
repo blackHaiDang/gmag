3fb064922ba306495530ce39f82be2b19bca4dd2
blackHaiDang <nguyenhaidang.business@gmail.com>
2023-02-16 15:55:56 +0700

    add nestjs hot reload and directus sdk

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { Directus } from '@directus/sdk';
+import { dir } from 'console';
+
+
+ const directus = new Directus(process.env.DIRECTUS_URL);
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello Dang';
+  }   
   }
-}
+
